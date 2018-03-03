10e878619a8fc0a24a631caa28996e368c3fba72
Rohit096 <35897843+Rohit096@users.noreply.github.com>
2018-03-04 01:38:04 +0530

    final version

--- app/src/main/java/com/example/rohitsingh/dashboard/Main2Activity.java
+++ app/src/main/java/com/example/rohitsingh/dashboard/Main2Activity.java
@@ -1,10 +1,8 @@
 package com.example.rohitsingh.dashboard;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,48 +10,137 @@ import android.widget.TextView;
 public class Main2Activity extends AppCompatActivity {
     Button team1;
     Button team2;
+    Button reset;
+    Button regain;
+    TextView vw;
     int a=0;
     int b=0;
+    int a1=0;
+    int b1=0;
+    public static final String team_a = "a";
+    public static final String team_b = "b";
+    public static final String set_a = "a1";
+    public static final String set_b = "b1";
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+         final SharedPreferences sharedpreferences= getPreferences(MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedpreferences.edit();
 
-       team1 = (Button) findViewById(R.id.button2);
+
+
+
+        team1 = (Button) findViewById(R.id.button2);
         team2 = (Button) findViewById(R.id.button3);
+        reset =(Button) findViewById(R.id.reset);
+        regain = (Button)findViewById(R.id.regain);
+        vw=(TextView) findViewById(R.id.view);
+
+        final TextView ta=(TextView) findViewById(R.id.textView7);
+        final TextView tb = (TextView) findViewById(R.id.textView8);
+
         final TextView t=(TextView) findViewById(R.id.textView6);
 
         team1.setOnClickListener(new View.OnClickListener() {
             @Override
 
             public void onClick(View view) {
+
                 a++;
-                TextView ta=(TextView) findViewById(R.id.textView7);
-                if (a>=0&&a<=25)
+
+                if (a>=0&&a<=25) {
+
                     ta.setText(""+a);
-                else
-                {ta.setText("Winner");
-                    t.setText("1:0");
+                    editor.putInt(team_a,a);
+                    editor.commit();
+
                 }
+                else
+                {
+                    vw.setText("Team A Wins");
+                    a1++;
+                    t.setText(a1+" : "+b1);
+
+                    a=0;
+                    b=0;
+                    ta.setText(""+a);
+                    tb.setText(""+b);
+                    editor.putInt(set_a,a1);
+                    editor.putInt(set_b,b1);
+
+                    editor.commit();
+                }
+
             }
-            
+
         });
         team2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 b++;
-                TextView tb = (TextView) findViewById(R.id.textView8);
-                if (b >= 0 && b <= 25)
+                if (b >= 0 && b <= 25){
                     tb.setText(""+b);
+                    editor.putInt(team_b,b);
+                    editor.apply();
+                }
                 else {
-                    tb.setText("Winner");
-                    t.setText("0:1");
+                    vw.setText("Team B Wins");
+                    b1++;
+                    t.setText(a1+" : "+b1);
+
+                    a=0;
+                    b=0;
+                    ta.setText(""+a);
+                    tb.setText(""+b);
+                    editor.putInt(set_a,a1);
+                    editor.putInt(set_b,b1);
+                    editor.commit();
                 }
             }
         });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ta.setText("0");
+                tb.setText("0");
+                a1=0;
+                b1=0;
+                t.setText(a1+" : "+b1);
+
+
+            }
+        });
+
+        regain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int temp_a=0;
+                int temp_b=0;
+
+                a = sharedpreferences.getInt(team_a,0);
+                b=sharedpreferences.getInt(team_b,0 );
+                ta.setText(""+a);
+                tb.setText(""+b);
+
+                temp_a=sharedpreferences.getInt(set_a,0);
+                temp_b = sharedpreferences.getInt(set_b, 0);
+                t.setText(temp_a+" : "+temp_b);
+
+            }
+        });
+
+
 
 
     }
 
 }
+
 
--- app/src/main/java/com/example/rohitsingh/dashboard/MainActivity.java
+++ app/src/main/java/com/example/rohitsingh/dashboard/MainActivity.java
@@ -5,16 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button b1;
+    TextView b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b1=(Button)findViewById(R.id.button);
+        b1=(TextView)findViewById(R.id.button);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
